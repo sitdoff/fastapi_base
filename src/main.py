979331e8fd9d6ec323afce009807c7c83d051ecc
Roman Ivanov <sitdoff@gmail.com>
2024-10-31 15:01:02 +0300

    update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
 from fastapi import FastAPI
+
+app = FastAPI()
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        "src.main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+    )
