030e6fea8a5584aeb9fd222c288990afcbb47a6e
Roman Ivanov <sitdoff@gmail.com>
2024-11-01 12:47:19 +0300

    update run app

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
 
+from api import router as api_router
+from core.config import settings
+
 app = FastAPI()
+app.include_router(api_router, prefix=settings.api_prifix.prefix)
 
 if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        "src.main:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True,
+        "main:app",
+        **settings.run.model_dump(),
     )
