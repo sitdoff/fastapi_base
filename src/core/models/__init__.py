ab45434ecc002a6c3023a5e8da377b6c9b0cf731
Roman Ivanov <sitdoff@gmail.com>
2024-11-06 19:12:33 +0300

    delete User

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -1,8 +1,6 @@
 __all__ = (
     "db_helper",
     "Base",
-    "User",
 )
 from .base import Base
 from .db_helper import db_helper
-from .user import User
--- src/core/models/user.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.orm import Mapped, mapped_column
-
-from .base import Base
-
-
-class User(Base):
-    username: Mapped[str] = mapped_column(unique=True)
