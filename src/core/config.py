8b28f362784232bd0d87ba6463f09d74c42baa1e
Roman Ivanov <sitdoff@gmail.com>
2024-11-06 19:47:00 +0300

    add naming_convention

--- src/core/config.py
+++ src/core/config.py
@@ -29,6 +29,14 @@ class DatabaseConfig(BaseSettings):
             url=f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
         )
 
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
--- src/core/models/base.py
+++ src/core/models/base.py
@@ -1,10 +1,14 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
 
+from core.config import settings
 from utils import camel_case_to_snake_case
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
